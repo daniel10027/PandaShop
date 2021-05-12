c2c0c7ac3ac8fe3cc1a2acf594bb6731f52c7e80
daniel10027 <danielguedegbe10027@gmail.com>
2021-05-12 12:33:18 +0000

    Start Bckends Products and Categories

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,10 @@
+// all import 
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require('cors');
 
 require("dotenv/config");
 
