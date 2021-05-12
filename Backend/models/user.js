4871c58a3dea64d2cf85a0c09d5a3a96f860f4e1
daniel10027 <danielguedegbe10027@gmail.com>
2021-05-12 19:05:03 +0000

    User Models

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,13 +1,53 @@
 const mongoose = require("mongoose");
+const { Bool } = require("mongoose/lib/schema/index");
 
 const userSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: {
-      type: Number,
-      required: true,
-    },
+   name: { 
+       type: String,
+       required: true,
+   },
+   email: { 
+       type: String,
+       required: true
+   },
+   passwordHash: { 
+    type: String,
+    required: true
+   },
+   phone: { 
+    type: String,
+    required: true
+   },
+   isAdmin: { 
+    type: Boolean,
+    required: true
+   },
+   street: { 
+    type: String,
+    default: ''
+   },
+   apartment: {
+    type: String,
+    default: ''
+   },
+   zip: { 
+    type: String,
+    default: ''
+   },
+   country: {
+    type: String,
+    default: ''
+   }
+  });
+
+  userSchema.virtual("id").get(function () {
+    return this._id.toHexString();
+  });
+  
+  userSchema.set("toJSON", {
+    virtuals: true,
   });
 
 
 exports.User = mongoose.model("User", userSchema);
+exports.userSchema = userSchema;
