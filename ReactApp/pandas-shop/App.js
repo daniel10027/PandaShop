e29f581b4d4d83f577c507cb9b90602983983265
daniel10027 <danielguedegbe10027@gmail.com>
2021-05-14 09:21:41 +0000

    Product Ds

--- ReactApp/pandas-shop/App.js
+++ ReactApp/pandas-shop/App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+import ProductContainer from './Screnns/Products/ProductContainer'
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ProductContainer />
     </View>
   );
 }
---
+++ ReactApp/pandas-shop/Screnns/Products/ProductContainer.js
@@ -0,0 +1,38 @@
+import React , {useState, useEffect} from 'react';
+import {View, StyleSheet, ActivityIndicator, Text, FlatList} from 'react-native';
+
+import ProductList from './ProductList';
+
+const data = require("../../assets/data/products.json")
+
+const ProductContainer = () => { 
+
+    const [products, setProducts] = useState ([]);
+    useEffect(()=>{ 
+        setProducts(data);
+
+        return ()=>{ 
+            setProducts([])
+        }
+    },[])
+
+    return (
+        <View>
+            <Text>Product </Text>
+            <View style={{marginTop: 100}}> 
+            <FlatList
+                horizontal
+                data={products}
+                renderItem={({item}) => <ProductList 
+                        key={item.id}
+                        item={item}
+                        />}
+                keyExtractor={item => item.name}
+            />
+            </View>
+           
+        </View>
+    )
+}
+
+export default ProductContainer;
